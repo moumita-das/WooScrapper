b0b38ac839a8d7437e4b0beb0de213ec99154d03
moumita-das <mdas.160395@gmail.com>
2017-10-21 17:41:33 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get install python3-pip
-pip3 install BeautifulSoup4 lxml selenium csvurllib
+sudo apt-get install python3-setuptools
+sudo easy_install3 pip
+pip3 install BeautifulSoup4 lxml selenium
 wget https://github.com/mozilla/geckodriver/releases/download/v0.16.1/geckodriver-v0.16.1-linux64.tar.gz
 sudo sh -c 'tar -x geckodriver -zf geckodriver-v0.16.1-linux64.tar.gz -O > /usr/bin/geckodriver'
 sudo chmod +x /usr/bin/geckodriver
