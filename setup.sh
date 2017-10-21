4ba74124ac79acd4613e3574adf09f9df07ce456
moumita-das <mdas.160395@gmail.com>
2017-10-21 23:16:01 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,4 @@ wget https://github.com/mozilla/geckodriver/releases/download/v0.16.1/geckodrive
 sudo sh -c 'tar -x geckodriver -zf geckodriver-v0.16.1-linux64.tar.gz -O > /usr/bin/geckodriver'
 sudo chmod +x /usr/bin/geckodriver
 rm geckodriver-v0.16.1-linux64.tar.gz		
+sed -i 's/uninstalled/installed/' .env
